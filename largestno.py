755b6670cdf12909124de46174d70655c76940a8
Meera.G <75059035+Meera-janani@users.noreply.github.com>
2020-12-11 12:04:46 +0530

    Update largestno.py

--- largestno.py
+++ largestno.py
@@ -1,7 +1,7 @@
 a=int(input("enter first number:"))
 b=int(input("enter the second number:"))
 c=int(input("enter the third number:"))
-if(a>b,a>c):
+if(a>b&a>c):
     print(a,"is the largest number.")
 elif(b>c):
     print(b,"is the largest number.")
